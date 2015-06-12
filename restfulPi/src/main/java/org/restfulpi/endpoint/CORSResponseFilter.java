b93df93627207f4fe3bf3eb82779830988e17758
Aaron Williams <aaronnw2@gmail.com>
2015-06-12 14:34:27 -0400

    CORS allow headers change

--- restfulPi/src/main/java/org/restfulpi/endpoint/CORSResponseFilter.java
+++ restfulPi/src/main/java/org/restfulpi/endpoint/CORSResponseFilter.java
@@ -15,6 +15,6 @@ public class CORSResponseFilter implements ContainerResponseFilter {
  
 		headers.add("Access-Control-Allow-Origin", "*");		
 		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");			
-		headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
+		headers.add("Access-Control-Allow-Headers", "*");
 	}
 }
