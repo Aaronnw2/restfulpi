dcd33400fd869c43240bd5fceaef1b81ae21861e
Aaron Williams <aaronnw2@gmail.com>
2015-06-14 09:02:23 -0400

    Added check to see if a pin was already provisioned

--- restfulPi/src/main/java/org/restfulpi/gpio/GPIORequestHandler.java
+++ restfulPi/src/main/java/org/restfulpi/gpio/GPIORequestHandler.java
@@ -46,6 +46,7 @@ public class GPIORequestHandler {
 
 	public HTTPResponse provisionPin(int pinNumber, String inName, String inInitialState) {
 		NumberedPin inPin = getNumberedPinFromNumber(pinNumber);
+		if(isProvisionedPin(pinNumber)) { return new HTTPResponse(false, format("Pin %d is already provisioned", pinNumber)); }
 		try {
 			OutputPin newPin = new OutputPin(inPin, inName,
 					controller.provisionDigitalOutputPin(inPin.getPin(), inName,
